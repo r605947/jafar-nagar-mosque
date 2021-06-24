a4d789e6fe48dbd9e460291398d112ef0425f318
r605947 <raselh739275@gmail.com>
2021-06-25 01:22:55 +0600

    first commit

---
+++ js/tooltip.js
@@ -0,0 +1,6 @@
+/*bootstarp-tooltip-start*/ 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+/*bootstarp-tooltip-end*/ 
